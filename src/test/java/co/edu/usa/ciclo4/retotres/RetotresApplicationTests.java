8d842e01fe203f2bbdbad484d0afddb54f120997
unknown <Formacion@SIBDFPSIBFT.SENA.RED>
2021-12-14 18:30:39 -0500

    Primer commit

---
+++ src/test/java/co/edu/usa/ciclo4/retotres/RetotresApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.ciclo4.retotres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetotresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
